f9697ee6075810d8de0125050e0d1a366a9bf6e4
mrsaxena77 <67632399+mrsaxena77@users.noreply.github.com>
2022-10-19 18:58:57 +0530

    Update PR-ALL_BASE_ADD.java

--- PR-ALL_BASE_ADD.java
+++ PR-ALL_BASE_ADD.java
@@ -1,40 +1,26 @@
 import java.util.*;
 
-public class ATA {
+public class DTA {
 
     public static Scanner scn = new Scanner(System.in);
 
-    public static int AbToDec(int n, int b) {
-        int ans = 0;
-        int pow = 1;
-
-        while (n != 0) {
-            int r = n % 10;
-            n /= 10;
-
-            ans += r * pow;         
-            pow *= b;
-        }
-        return ans;
-    }
-
-    public static int DecToAb(int n, int b) {
+    public static int anybaseReturn(int n, int b) {
         int ans = 0;
         int pow = 1;
 
         while (n != 0) {
             int r = n % b;
-            n /= b;
+            n = n / b;
 
             ans += r * pow;
-            pow *= 10;
+            pow = pow * 10;
         }
         return ans;
     }
 
     public static void main(String[] args) {
-        int n = scn.nextInt();
-        int returnDec = AbToDec(n, scn.nextInt());
-        System.out.print(DecToAb(returnDec, scn.nextInt()));
+        int a = anybaseReturn(scn.nextInt(), scn.nextInt());
+        System.out.print(a);
     }
+
 }
